a4f03f152d57e0fc28830a78ee82b11e834136c4
andrew-asana <andrewwilliams@asana.com>
2023-01-05 15:25:54 -0800

    removed submit logic from the rule save file for now

--- pages/api/rule/saveFile/submit.js
+++ pages/api/rule/saveFile/submit.js
@@ -28,36 +28,44 @@ const handler = async (req, res) => {
 
   // Retrieve selected data from AppComponent Modal: save file path ID
   const { values } = dataParsed;
-  const taskGid = dataParsed.task;
+  const projectGid = dataParsed.project;
   const saveFilePath = values && values.single_line_text_field_1;
 
-  // Get the task data to determine task type
-  const asanaConfig = {
-    headers: constants.asanaRequestHeaders,
-  };
-  const response = await axios.get(`${constants.asanaApiUrl}/tasks/${taskGid}`, asanaConfig);
-  const taskData = response && response.data && response.data.data;
-  const isApprovalTask = taskData && taskData.resource_subtype === 'approval';
+  // TODO: Implement handling the save file path per project
+  // that the rule is added. Consider either saving at the project
+  // custom field level (requires being in a portfolio) or somewhere
+  // else at the project level. This save file path needs to be set
+  // per where this rule is configured per project.
+  // This save file path most likely needs to be the Bynder instance specific
+  // to the user's Bynder domain as well as the Bynder token.
 
-  // Return if this isn't an approval task or the save file path is missing
-  if (!isApprovalTask || !saveFilePath || saveFilePath.length <= 0) {
-    res.status(200).json({
-      error: 'Missing correct task data for save file',
-    });
-    return;
-  }
+  // // Get the task data to determine task type
+  // const asanaConfig = {
+  //   headers: constants.asanaRequestHeaders,
+  // };
+  // const response = await axios.get(`${constants.asanaApiUrl}/tasks/${taskGid}`, asanaConfig);
+  // const taskData = response && response.data && response.data.data;
+  // const isApprovalTask = taskData && taskData.resource_subtype === 'approval';
 
-  // Update the trigger task's Bynder Asset URL custom field
-  const customFields = {};
-  customFields[PROJECT_DAM_ASSETS_REVIEW_CF_PATH] = saveFilePath;
-  await axios.put(`${constants.asanaApiUrl}/tasks/${taskGid}`, {
-    data: {
-      custom_fields: customFields,
-    },
-  }, asanaConfig).catch((error) => {
-    console.log(error);
-    throw new Error(error);
-  });
+  // // Return if this isn't an approval task or the save file path is missing
+  // if (!isApprovalTask || !saveFilePath || saveFilePath.length <= 0) {
+  //   res.status(200).json({
+  //     error: 'Missing correct task data for save file',
+  //   });
+  //   return;
+  // }
+
+  // // Update the trigger task's Bynder Asset URL custom field
+  // const customFields = {};
+  // customFields[PROJECT_DAM_ASSETS_REVIEW_CF_PATH] = saveFilePath;
+  // await axios.put(`${constants.asanaApiUrl}/tasks/${taskGid}`, {
+  //   data: {
+  //     custom_fields: customFields,
+  //   },
+  // }, asanaConfig).catch((error) => {
+  //   console.log(error);
+  //   throw new Error(error);
+  // });
 
   res.status(200).json({});
 };
--- utils/asana.js
+++ utils/asana.js
@@ -1,16 +1,16 @@
-async function getCustomFieldValueByName(taskData, customFieldName) {
+function getCustomFieldValueByName(taskData, customFieldName) {
   const customFields = taskData && taskData.custom_fields;
   const result = customFields.find((field) => field.name === customFieldName);
   return result.display_value;
 }
 
-async function getCustomFieldValueByGid(taskData, customFieldGid) {
+function getCustomFieldValueByGid(taskData, customFieldGid) {
   const customFields = taskData && taskData.custom_fields;
   const result = customFields.find((field) => field.gid === customFieldGid);
   return result.display_value;
 }
 
-async function getExternalAttachmentGid(attachmentData) {
+function getExternalAttachmentGid(attachmentData) {
   const result = attachmentData.find((attachment) => attachment.resource_subtype === 'external');
   return result.gid;
 }
