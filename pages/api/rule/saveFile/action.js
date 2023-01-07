105f8327ffbcb04e76271b17f2da80bef1c97a04
andrew-asana <andrewwilliams@asana.com>
2023-01-06 16:43:56 -0800

    testing alternative

--- pages/api/rule/saveFile/action.js
+++ pages/api/rule/saveFile/action.js
@@ -167,7 +167,14 @@ const handler = async (req, res) => {
 
   // TODO: Implement loop to handle making requests
   // until itemsDone from the response matches items in query parameters
-  const pollResponse = await axios.get(`${constants.bynderApiUrl}/v4/upload/poll?items=${importId}&`, bynderConfig);
+  // const pollResponse = await axios.get(`${constants.bynderApiUrl}/v4/upload/poll?items=${importId}&`, bynderConfig);
+  const config = {
+    params: { items: importId },
+    headers: {
+      Authorization: constants.bynderRequestHeaders.Authorization,
+    },
+  };
+  const pollResponse = await axios.get(`${constants.bynderApiUrl}/v4/upload/poll?items/`, config);
   console.log(`Received poll response as: ${JSON.stringify(pollResponse.data)}`);
 
   //   h. Save as a new asset
