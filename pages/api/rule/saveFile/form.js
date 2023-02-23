a65be62237ec7ed41008300c8e998e23b7e021d0
andrew-asana <andrewwilliams@asana.com>
2023-02-23 15:40:51 -0800

    added https to url

--- pages/api/rule/saveFile/form.js
+++ pages/api/rule/saveFile/form.js
@@ -6,7 +6,7 @@ const handler = (req, res) => {
       template: 'form_metadata_v0',
       title: 'Add Save File Action',
       metadata: {
-        on_submit_callback: `${constants.vercelUrl}/api/rule/saveFile/submit`,
+        on_submit_callback: `https://${constants.vercelUrl}/api/rule/saveFile/submit`,
         fields: [
           {
             name: 'Save the attached Bynder asset to the Bynder workspace Assets folder.',
