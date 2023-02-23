cb59e5449c054e76c1143c555316e61397aa9893
andrew-asana <andrewwilliams@asana.com>
2023-02-23 15:01:52 -0800

    added back hardcoded url

--- pages/api/rule/saveFile/form.js
+++ pages/api/rule/saveFile/form.js
@@ -1,4 +1,4 @@
-const constants = require('../../../../constants');
+// const constants = require('../../../../constants');
 
 const handler = (req, res) => {
   res.status(200).json(
@@ -6,7 +6,7 @@ const handler = (req, res) => {
       template: 'form_metadata_v0',
       title: 'Add Save File Action',
       metadata: {
-        on_submit_callback: `${constants.vercelUrl}/api/rule/saveFile/submit`,
+        on_submit_callback: 'https://dam-showcase-andrew-git-dam-showcase-update-andrew-asana.vercel.app/api/rule/saveFile/submit',
         fields: [
           {
             name: 'Save the attached Bynder asset to the Bynder workspace Assets folder.',
