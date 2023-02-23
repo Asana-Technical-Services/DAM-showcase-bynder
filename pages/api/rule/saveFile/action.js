50602688092f8e4bd5befa387ad4de748822952a
andrew-asana <andrewwilliams@asana.com>
2023-02-22 17:17:06 -0800

    debug commit

--- pages/api/rule/saveFile/action.js
+++ pages/api/rule/saveFile/action.js
@@ -83,9 +83,7 @@ const handler = async (req, res) => {
   // const initResponse = await axios.post(`${constants.bynderApiUrl}/upload/init`, formData, bynderMultiPartConfig);
   // const params = initResponse && initResponse.data && initResponse.data.multipart_params;
   const initializedData = await bynder.initializeUpload(assetName);
-  console.log(`[DEBUG] initialized data: ${JSON.stringify(initializedData)}`);
-  return;
-  if (!initializedData || !initializedData.multpart_params) {
+  if (!initializedData) {
     res.status(200).json({
       error: 'Failed to initialize upload to Amazon S3 endpoint',
     });
