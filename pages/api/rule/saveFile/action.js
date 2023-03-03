f7c45cdcedaa0b2d0bcfbc78bcd647cae3694053
andrew-asana <andrewwilliams@asana.com>
2023-03-03 15:59:26 -0800

    added back assetName check since it should exist based off the attachment name

--- pages/api/rule/saveFile/action.js
+++ pages/api/rule/saveFile/action.js
@@ -38,7 +38,7 @@ const handler = async (req, res) => {
   const attachmentGid = asanaUtils.getCustomFieldValueByName(taskData, 'Bynder Asset Attachment GID');
 
   // Return if this isn't an approval task
-  if (!isApprovalTask || !attachmentGid) {
+  if (!isApprovalTask || !assetName || !attachmentGid) {
     res.status(200).json({
       error: 'Missing correct task data for save file',
     });
