02fd70e8c903e77e41faacadbbda91b1cf134e0f
Ben Graney Green <bengraneygreen@asana.com>
2024-07-10 10:29:51 -0700

    Update action.js

--- pages/api/rule/saveFile/action.js
+++ pages/api/rule/saveFile/action.js
@@ -8,7 +8,7 @@ const constants = require('../../../../constants');
 const asanaUtils = require('../../../../utils/asana');
 const bynder = require('../../../../bynder');
 
-const handler = async (req, res) => {
+export async function POST(req, res) {
   const { data } = req.body;
   console.log(data)
   if (!data) {
@@ -108,5 +108,3 @@ const handler = async (req, res) => {
     ],
   });
 };
-
-module.exports = handler;
